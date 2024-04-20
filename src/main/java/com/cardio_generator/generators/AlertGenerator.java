0b001a632e3cb0d753f4a91dffe5cf29dd785101
Marta <m.adamowska@student.maastrichtuniversity.nl>
2024-04-20 18:13:33 +0200

    review and comment addition

--- src/main/java/com/cardio_generator/generators/AlertGenerator.java
+++ src/main/java/com/cardio_generator/generators/AlertGenerator.java
@@ -1,19 +1,30 @@
 package com.cardio_generator.generators;
 
 import java.util.Random;
-
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * Implementation of PatientDataGenerator for Alert Generator
+ */
 public class AlertGenerator implements PatientDataGenerator {
     // Changed variable name to UPPER_SNAKE_CASE
     public static final Random RANDOM_GENERATOR = new Random();
     // Changed variable name to lowerCamelCase
     private boolean[] alertStates; // false = resolved, true = pressed
 
+    /**
+     * Constructs an Allert Generator with a specific number of patients.
+     * @param patientCount The number of patients for which alert states are measured.
+     */
     public AlertGenerator(int patientCount) {
         alertStates = new boolean[patientCount + 1];
     }
 
+    /**
+     * Generates alert data for a particular patient and outputs it using a provided output strategy.
+     * @param patientId      An integer representation of a patient's ID.
+     * @param outputStrategy An object of type OutputStrategy that specifies the strategy used in generation.
+     */
     @Override
     public void generate(int patientId, OutputStrategy outputStrategy) {
         try {
--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,14 @@
 package com.cardio_generator.outputs;
-
+/**
+* Interface for specifying the output strategy.
+ */
 public interface OutputStrategy {
+    /**
+     * Outputs patient data.
+     * @param patientId     An integer representation of a patient's ID.
+     * @param timestamp     A long representation of the timestamp
+     * @param label         A string representation of a label of the data.
+     * @param data          A string representation of the data to be outputted.
+     */
     void output(int patientId, long timestamp, String label, String data);
 }
